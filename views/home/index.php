f0e031a1b43f92afcc229aae2014c10cf48358dd
Quinton Ward <neoaptt@gmail.com>
2017-09-13 22:25:09 -0400

    Update index.php

--- views/home/index.php
+++ views/home/index.php
@@ -2,8 +2,11 @@
     <div class="row">
         <div class="col-sm-12">
             <?php
+            /*this is where we access the information that the controller handed to us*/
             if (is_array($viewbag['ourVar'])) {
+                /*we assume it is the data from the database*/
                 foreach ($viewbag['ourVar'] as $item) {
+                    /*the database had test_id and description and it's two fields we are instrested in printing to the html page*/
                     echo $item['test_id'] . ': ' . $item['description'];
                 }
             }
